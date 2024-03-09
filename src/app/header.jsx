d9c560b4df9511d93b3baadeea0ffef9f9d7354d
ChrAlpha <53332481+ChrAlpha@users.noreply.github.com>
2024-03-09 12:11:17 +0800

    feat(header): rss icon in header

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -46,6 +46,23 @@ const Header = () => {
           <NavBar responsive="hidden md:flex" />
           <div className="relative flex flex-row gap-4 items-center">
             <ToggleTheme />
+            <Link
+              className="text-[#666] dark:text-[#888] hover:text-black dark:hover:text-white focus:outline-none"
+              href="/rss"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3.75 4.5a.75.75 0 0 1 .75-.75h.75c8.284 0 15 6.716 15 15v.75a.75.75 0 0 1-.75.75h-.75a.75.75 0 0 1-.75-.75v-.75C18 11.708 12.292 6 5.25 6H4.5a.75.75 0 0 1-.75-.75V4.5Zm0 6.75a.75.75 0 0 1 .75-.75h.75a8.25 8.25 0 0 1 8.25 8.25v.75a.75.75 0 0 1-.75.75H12a.75.75 0 0 1-.75-.75v-.75a6 6 0 0 0-6-6H4.5a.75.75 0 0 1-.75-.75v-.75Zm0 7.5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </Link>
           </div>
         </div>
       </header>
