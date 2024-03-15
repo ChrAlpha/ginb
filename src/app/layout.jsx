67a4ad444ab4efb2a9e9bb46952cd6ee47975124
ChrAlpha <53332481+ChrAlpha@users.noreply.github.com>
2024-03-15 21:46:09 +0800

    refactor(app/layout): viewport enable user scalability

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -49,7 +49,7 @@ export const viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 5,
-  userScalable: false,
+  userScalable: true,
 };
 
 export default function RootLayout({ children }) {
