99888011e8b9c3aa74077196388ea513dde9cac3
ChrAlpha <53332481+ChrAlpha@users.noreply.github.com>
2024-03-21 19:43:18 +0800

    feat(rss): feed file cache headers

--- src/app/rss/route.js
+++ src/app/rss/route.js
@@ -8,6 +8,9 @@ export async function GET() {
   return new Response(feed, {
     headers: {
       "Content-Type": "application/xml",
+      "Cache-Control": "s-maxage=86400, stale-while-revalidate=3600",
+      "CDN-Cache-Control": "max-age=86400",
+      "Vercel-CDN-Cache-Control": "max-age=86400",
     },
   });
 }
